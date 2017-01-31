aa877802b5ddac347cf3dba2974a882be1e9e15e
John Miller <jmiller@rjmetrics.com>
2017-01-31 10:32:58 -0500

    using export endpoint

--- stream_referral_saasquatch.py
+++ stream_referral_saasquatch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import csv
 import datetime
 import json
 import logging
@@ -13,31 +14,23 @@ import dateutil.parser
 import requests
 import stitchstream
 
-
 QUIET = False
 API_KEY = None
-ALIAS = None
-BASE_URL = "https://app.referralsaasquatch.com/api/v1/{alias}"
-GET_COUNT = 0
-PERSIST_COUNT = 0
+TENANT_ALIAS = None
+BASE_URL = "https://app.referralsaasquatch.com/api/v1/{tenant_alias}"
 DATETIME_FMT = "%Y-%m-%dT%H:%M:%SZ"
+DEFAULT_START_DATE = datetime.datetime(2016, 1, 1).strftime(DATETIME_FMT)
 
 state = {
-    "users": None,
-    "accounts": None,
-    "codes": None,
-    "referrals": None,
-    "balances": None,
-    "rewards": None,
+    "users": DEFAULT_START_DATE,
+    "reward_balances": DEFAULT_START_DATE,
+    "referrals": DEFAULT_START_DATE,
 }
 
-endpoints = {
-    "users": "/users",
-    "accounts": "/account/{account_id}",
-    "codes": "/code/{code_id}",
-    "referrals": "/referrals",
-    "balances": "/reward/balance",
-    "rewards": "/reward",
+entity_export_types = {
+    "users": "USER",
+    "reward_balances": "REWARD_BALANCE",
+    "referrals": "REFERRAL",
 }
 
 logging.config.fileConfig("/etc/stitch/logging.conf")
@@ -68,82 +61,152 @@ def stream_records(entity, records):
 
 def load_config(config_file):
     global API_KEY
-    global ALIAS
+    global TENANT_ALIAS
     global BASE_URL
 
     with open(config_file) as f:
         config = json.load(f)
 
     API_KEY = config['api_key']
-    ALIAS = config['tenant_alias']
-    BASE_URL = BASE_URL.format(alias=ALIAS)
-
-    logger.debug("Loaded config. alias={}".format(ALIAS))
+    TENANT_ALIAS = config['tenant_alias']
+    BASE_URL = BASE_URL.format(alias=TENANT_ALIAS)
 
 
 def load_state(state_file):
+    logger.info("Loading state from " + args.state)
     with open(state_file) as f:
         data = json.load(f)
 
     state.update(data)
+    logger.info("State loaded. {}".format(state))
 
 
 def load_schema(entity):
-    with open("stream_referral_saasquatch/{}.json".format(entity)):
+    with open("stream_referral_saasquatch/{}.json".format(entity)) as f:
         return json.load(f)
 
 
-@backoff.on_exception(backoff.expo,
-                      (requests.exceptions.RequestException),
-                      max_tries=5,
-                      giveup=lambda e: e.response is not None and 400 <= e.response.status_code < 500,
-                      factor=2)
-def request(url, params=None):
-    global GET_COUNT
-    params = params or {}
-
-    logger.debug("Making request: GET {} {}".format(url, params))
-    response = session.get(url, params=params, auth=("notused", API_KEY))
-    logger.debug("Got response code: {}".format(response.status_code))
-
-    GET_COUNT += 1
-    response.raise_for_status()
-    return response
+def export_ready(export_id):
+    url = BASE_URL + "/export/{}".format(export_id)
+    auth = ("", API_KEY)
+    headers = {'Content-Type': "application/json"}
+    resp = session.get(url, auth=auth, headers=headers)
+    result = resp.json()
+    return result['status'] == 'COMPLETED"
 
 
-def get_url_and_params(entity, **kwargs):
-    url = BASE_URL + endpoints[entity]
-    params = {k: v for k, v in kwargs.items() if k not in url}
-    url = url.format(**kwargs)
-    return (url, params)
+def request_export(entity):
+    url = BASE_URL + "/export"
+    auth = ("", API_KEY)
+    headers = {'Content-Type': "application/json"}
+    data = {
+        "type": entity_export_types[entity],
+        "format": "CSV",
+        "name": "Stitch Streams {}:{}".format(entity, datetime.datetime.utcnow()),
+        "params": {
+            "createdOrUpdatedSince": state[entity],
+        },
+    }
+
+    resp = session.post(url, auth=auth, headers=headers, json=data)
+    result = resp.json()
+
+    if 'id' in result:
+        waited = 0
+        while True:
+            if export_ready(result['id']):
+                return result['id']
+
+            if (waited > 3600):
+                raise Exception("{} export took over an hour to complete. Aborting."
+                                .format(entity))
+
+            time.sleep(5)
+            waited += 5
+
+    else:
+        raise Exception("Request to create {} export failed: {} - {}"
+                        .format(entity, resp.status_code, resp.content))
 
 
-def get_paged_list(entity, **kwargs):
-    url, params = get_url_and_params(entity, **kwargs)
+def stream_export(entity, export_id):
+    url = BASE_URL + "/export/{}/download".format(export_id)
+    auth = ("", API_KEY)
+    headers = {'Content-Type': "application/json"}
+    resp = session.get(url, auth=auth, headers=headers, stream=True)
 
-    params['limit'] = 100
-    params['offset'] = 0
+    lines = [line.decode("utf-8") for line in r.iter_lines()]
 
-    count = 0
-    items = []
-    while True:
-        resp = request(url, params)
-        data = resp.json()
+    fields = None
+    rows = []
+    for line in r.iter_lines():
+        line = line.decode("utf-8")
+        split = line.split(",")
 
-        count += len(data[entity])
-        items.extend(data[entity])
-        if count >= data['count']:
-            break
+        if not fields:
+            fields = split
         else:
-            params['offset'] += params['limit']
+            row = dict(zip(fields, split))
+            rows.append(row)
 
-    return items
+    return rows
 
 
-def get(entity, **kwargs):
-    url, params = get_url_and_params(entity, **kwargs)
-    resp = request(url, params)
-    return resp.json()
+def transform_timestamp(value):
+    if not value:
+        return None
+
+    dt = datetime.datetime.utcfromtimestamp(int(value) * 0.001)
+    return dt.strftime(DATETIME_FMT)
+
+
+TRANSFORMS = {
+    "users": {
+        "dateCreated": transform_timestamp,
+    },
+    "reward_balances": {
+        "amount": int,
+    },
+    "referrals": {
+        "dateReferralStarted": transform_timestamp,
+        "dateReferralPaid": transform_timestamp,
+        "dateReferralEnded": transform_timestamp,
+        "dateModerated": transform_timestamp,
+    },
+}
+
+
+def transform_row(entity, row):
+    for field, transform in TRANSFORMS[entity].items():
+        if field in row:
+            row[field] = transform(row[field])
+
+    return row
+
+
+def sync_entity(entity):
+    schema = load_schema(entity)
+    stream_schema(entity, schema)
+
+    try:
+        export_start = datetime.datetime.utcnow().strftime(DATETIME_FMT)
+        export_id = request_export(entity)
+    except Exception as e:
+        logger.fatal(e.message)
+        sys.exit(-1)
+
+    rows = stream_export(entity, export_id)
+    rows = [transform_row(row, schema) for row in rows]
+    stream_records(entity, rows)
+
+    state[entity] = export_start
+    stream_state()
+
+
+def do_sync():
+    sync_entity("users")
+    sync_entity("reward_balances")
+    sync_entity("referrals")
 
 
 def do_check():
@@ -151,121 +214,9 @@ def do_check():
         pass
     except requests.exceptions.RequestException as e:
         logger.fatal("Error checking connection using {e.request.url}; "
-                     "received status {e.response.status_code}: {e.response.test}".format(e=e))
+                     "received status {e.response.status_code}: {e.response.test}"
+                     .format(e=e))
         sys.exit(-1)
-
-
-def transform_timestamp(timestamp, iso=True):
-    dt = datetime.datetime.utcfromtimestamp(int(timestamp) * 0.001)
-    if iso:
-        dt = dt.strftime(DATETIME_FMT)
-
-    return dt
-
-
-def transform_array(values, schema):
-    return [transform(value, schema) for value in values]
-
-
-def transform_object(value, schema):
-    return {field_name: transform(value[field_name], field_schema)
-            for field_name, field_schema in schema.items()
-            if field_name in value}
-
-
-def transform(value, schema):
-    if not value:
-        if "null" in schema['type']:
-            return value
-        else:
-            raise ValueError("Value is null and null is not all allowed type")
-
-    if schema['type'] == "array" or "array" in schema['type']:
-        return transform_array(value, schema['items'])
-
-    if schema['type'] == "object" or "object" in schema['type']:
-        return transform_object(value, schema['properties'])
-
-    if "integer" in schema['type']:
-        value = int(value)
-
-    if "number" in schema['type']:
-        value = float(value)
-
-    if "format" in schema:
-        if schema['format'] == "date-time":
-            value = transform_timestamp(value)
-
-    return value
-
-
-def _flat(d, new_key, old_key, data_key):
-    data = d.pop(old_key, {})
-    d[new_key] = data[data_key]
-
-
-def do_sync():
-    schemas = {entity: load_schema(entity) for entity in endpoints.keys()}
-
-    users = get_paged_list("users")
-    account_ids = set()
-    referral_codes = set()
-
-    transformed_users = []
-    for user in users:
-        u = transform(user, schemas["users"])
-        account_ids.add(u['accountId'])
-        referral_codes.add(u['referralCode'])
-        transformed_users.append(u)
-
-    stream_schema("users", schemas["users"])
-    stream_records("users", transformed_users)
-
-    accounts = []
-    balances = []
-    rewards = []
-    for account_id in account_ids:
-        account = get("accounts", account_id=account_id)
-        account['referral'] = account.get('referral', {}).get('code')
-        account = transform(account, schemas["account"])
-        accounts.append(account)
-
-        account_balances = get("balances", accountId=account_id)
-        balances.extend(transform(i, schema["balances"]) for i in account_balances)
-
-        account_rewards = get("rewards", accountId=account_id)
-        rewards.extend(transform(i, schema["rewards"]) for i in account_rewards)
-
-    stream_schema("accounts", schema["accounts"])
-    stream_records("accounts", accounts)
-
-    stream_schema("balances", schema["balances"])
-    stream_records("balances", balances)
-
-    stream_schema("rewards", schema["rewards"])
-    stream_records("rewards", rewards)
-
-    codes = []
-    for code_id in referral_codes:
-        code = get("codes", code_id=code_id)
-        code = transform(code, schemas["codes"])
-        codes.append(code)
-
-    stream_schema("codes", schema["codes"])
-    stream_records("codes", codes)
-
-    referrals = get_paged_list("referrals")
-    transformed_referrals = []
-    for referral in referrals:
-        referral['referredUserId'] = referral.pop('referredUser')['id']
-        referral['referrerUserId'] = referral.pop('referrerUser')['id']
-        referral['referredRewardId'] = referral.pop('referredReward')['id']
-        referral['referrerRewardId'] = referral.pop('referrerReward')['id']
-        r = transform(referral, schema["referrals"])
-        transformed_referrals.append(r)
-
-    stream_schema("referrals", schema["referrals"])
-    stream_records("referrals", transformed_referrals)
 
 
 def main():
@@ -289,7 +240,6 @@ def main():
 
     load_config(args.config)
     if args.state:
-        logger.info("Loading state from " + args.state)
         load_state(args.state)
 
     if args.func == "check":
