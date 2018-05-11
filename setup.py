f5b38374a1358218b36fc43725db8fb84b1d5dfd
Nick McCoy <nick@stitchdata.com>
2018-05-11 14:49:28 -0400

    bumping version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-referral-saasquatch',
-      version='0.5.12',
+      version='1.0.0',
       description='Singer.io tap for extracting data from the Referral SaaSquatch API',
       author='Stitch',
       url='https://singer.io',
