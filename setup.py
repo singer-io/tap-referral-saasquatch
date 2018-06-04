5f07bf5cf4536d8a32d4b80d51ba1d27b0e7fa8e
Anne Maiale <anne@stitchdata.com>
2018-06-04 15:26:09 -0400

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-referral-saasquatch',
-      version='1.0.0',
+      version='1.0.1',
       description='Singer.io tap for extracting data from the Referral SaaSquatch API',
       author='Stitch',
       url='https://singer.io',
