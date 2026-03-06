9be31363127d21daf0cd66bb63beb117b9b01da0
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-03-06 11:49:38 +0530

    bump the version

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-referral-saasquatch',
-      version='2.0.0',
+      version='2.1.0',
       description='Singer.io tap for extracting data from the Referral SaaSquatch API',
       author='Stitch',
       url='https://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_referral_saasquatch'],
       install_requires=[
-          'singer-python==6.1.1',
-          'requests==2.32.4',
+          'singer-python==6.8.0',
+          'requests==2.32.5',
           'backoff==2.2.1',
       ],
       extras_require={
